69c854dd1912fe3af72c823aaaa4a5243e69003e
sb7916696-hash <sb7916696@gmail.com>
2026-03-15 20:11:45 +0530

    from src/constants

---
+++ testVoters.ts
@@ -0,0 +1,27 @@
+export const testVoters = [
+  {
+    name: "Arjun Sharma",
+    aadhaarNumber: "2345 6789 0123",
+    walletHint: "Account 1"
+  },
+  {
+    name: "Priya Nair", 
+    aadhaarNumber: "3456 7890 1234",
+    walletHint: "Account 2"
+  },
+  {
+    name: "Mohammed Faiz",
+    aadhaarNumber: "4567 8901 2345", 
+    walletHint: "Account 3"
+  },
+  {
+    name: "Sneha Patel",
+    aadhaarNumber: "5678 9012 3456",
+    walletHint: "Account 4"
+  },
+  {
+    name: "Ravi Kumar",
+    aadhaarNumber: "6789 0123 4567",
+    walletHint: "Account 5"
+  }
+];
